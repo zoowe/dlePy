8eaf72f950add4a2ac33658f8c5465f09a48dc52
Duy Le <ttduyle@gmail.com>
2018-06-11 11:45:54 -0400

    Add getdata for vasp
    only energy now

---
+++ vasp/getdata.py
@@ -0,0 +1,14 @@
+import gzip as gz
+
+def get_enegry( outcar ):
+    if '.gz' in outcar:
+        with gz.open( outcar, 'r' ) as f:
+            lines = f.readlines( )
+    else:
+        with open( outcar, 'r' ) as f:
+            lines = f.readlines( )
+    for iline in range( len( lines ) - 1, -1, -1 ):
+        if "energy  w" in lines[ iline ]:
+            ener = float( lines[ iline ].split( )[ 6 ] )
+            break
+    return ener
