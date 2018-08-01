6ad9ab6134af105c96bffc9b7f7afe14dcc30190
Duy Le <ttduyle@gmail.com>
2018-08-01 14:09:02 -0400

    change reso

--- TUTORIALS/VISUALIZATION/POVRAY/second_image.py
+++ TUTORIALS/VISUALIZATION/POVRAY/second_image.py
@@ -4,5 +4,5 @@ from ase.io import write
 benzene = molecule( 'C6H6' )
 
 write( 'second_image.pov', benzene, format = 'pov', run_povray = True, 
-        canvas_width = 300 # Set resolution of image's width to 300 pixels
+        canvas_width = 150 # Set resolution of image's width to 300 pixels
      )
