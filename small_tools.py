3b53af58da77f9ac61bc896cd874d81221d8e06c
Duy Le <ttduyle@gmail.com>
2018-10-22 15:22:02 -0400

    add small tools

---
+++ small_tools.py
@@ -0,0 +1,15 @@
+def file_exist(FILENAME):
+    """
+    Check if FILENAME exists, is readable
+    """
+    from os import path, access, R_OK
+
+    if path.exists(FILENAME) and path.isfile(FILENAME) and access(FILENAME, R_OK):
+        file_exists=True
+        txt='File '+FILENAME+' exists and readable. You are good to go'
+    else:
+        file_exists=False
+        txt='File '+FILENAME+' does not exist or is not readable.'
+    
+    return file_exists, txt  
+
