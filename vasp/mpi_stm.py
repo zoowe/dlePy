e1636b0761618737bf48b310718e1a5f2fd5bdea
Duy Le <ttduyle@gmail.com>
2018-10-22 15:23:14 -0400

    Upload STM code
    Not cleaned up yet

---
+++ vasp/mpi_stm.py
@@ -0,0 +1,218 @@
+"""
+Has not been cleaned up 
+"""
+"""
+***********************************
+Tersoff-Hamann approximation of STM
+
+Usage:
+from dlescripts.getstm import tersoff_hamann
+
+tersoff_hamann('PARCHG','OUTPUT_PREFIX',TOP,LDOS_IN,ref)
+
+************************************
+"""
+#!/usr/bin/env python
+from ase import *
+from ase.io import *
+import numpy as np
+from dlePy.vasp.chgcar import *
+from dlePy.small_tools import file_exist
+from mpi4py import MPI
+import sys
+import numpy as np
+from datetime import datetime
+
+comm = MPI.COMM_WORLD
+size = comm.Get_size()
+rank = comm.Get_rank()
+name = MPI.Get_processor_name()
+
+def print_mpi(rank,txt,endline=True):
+    if rank == 0:
+        if endline==True:
+            print txt
+        else:
+            print txt,
+
+def print_time(rank, time):
+    print_mpi (rank,'Total time: '+str(time))
+
+def tersoff_hamann(CONTCAR,INDATA,OUTPRE,TOP,LDOS_IN,ref):    
+    from scipy.interpolate import interp1d
+   
+    comm = MPI.COMM_WORLD
+    size = comm.Get_size()
+    rank = comm.Get_rank()
+    name = MPI.Get_processor_name()
+    print_mpi(rank,'***********************************')
+    print_mpi(rank,'Tersoff-Hamann approximation of STM')
+    print_mpi(rank,'Run with '+str(size)+' cpus on'+str( name))
+    print_mpi(rank,'The constant current STM image is  ')
+    print_mpi(rank,'simulated for the following:')
+    print_mpi(rank,''    )
+    print_mpi(rank,'Input data: '+INDATA)
+    print_mpi(rank,'Value of LDOS: '+str(LDOS_IN)+' e/A^3')
+    print_mpi(rank,'The program will add '+str(TOP)+' point')
+    print_mpi(rank,'in z directions')
+    print_mpi(rank,'STM image in CHGCAR format is written')
+    print_mpi(rank,'in '+OUTPRE+'.STM.vasp')
+    print_mpi(rank,'and in 2D crystal coordinates in: '+OUTPRE+'.STM')
+    print_mpi(rank,'')
+    print_mpi(rank,'************************************')
+    print_mpi(rank,'')
+    print_mpi(rank,'')
+    #Read CONTCAR to define the system.
+    print_mpi(rank,"checking file: "+CONTCAR)
+    file_exists, txt=file_exist(CONTCAR)
+    if not file_exists:
+        print_mpi(rank,txt)
+    else:
+        print_mpi(rank,"checking file: "+INDATA)
+        file_exists, txt=file_exist(INDATA)
+        if not file_exists:
+            print_mpi(rank,txt)
+#    file_exists = comm.bcast(file_exists,root=0)
+    if file_exists:
+        system=read(CONTCAR)
+        #del system.constraints
+    
+    
+    #We don't need this, thus we need to count how many lines
+    #we need to ignore
+        nline=9
+        startline=nline+system.get_number_of_atoms()
+   
+    # Reading INDATA
+        print_mpi ( rank, 'Reading '+INDATA+' ...')
+        t1=datetime.now() 
+    #Open file LOCPOT
+        file=open(INDATA,'r')
+    
+    #Read all ignored lines
+        for i in range(startline):
+            dump=file.readline()
+    
+    
+        ngr=file.readline().split()
+    #Make it becomes integers 
+        ng = (int(ngr[0]), int(ngr[1]), int(ngr[2]))
+        print_mpi(rank,'Grid info: '+str(ng[0]) + ' '+str(ng[1])+' '+str(ng[2]))
+        total0 = np.empty(ng)
+        for zz in range(total0.shape[2]):
+            for yy in range(total0.shape[1]):
+                total0[:, yy, zz] = np.fromfile(file, count = total0.shape[0],
+                                           sep=' ')
+    
+        file.close()
+        min=np.min(total0)
+        max=np.max(total0)
+        print_mpi(rank, 'Min ldos '+str(min/system.get_volume())+' e/A^3')
+        print_mpi(rank, 'Max ldos '+str(max/system.get_volume())+' e/A^3')
+        total = np.zeros([ng[0],ng[1],2*ng[2]])
+        total[:,:,0:ng[2]]=total0[:,:,0:ng[2]]
+        total[:,:,ng[2]:2*ng[2]]=total0[:,:,0:ng[2]]
+        del total0
+        #Done reading data
+        t2=datetime.now()
+        print_time (rank, t2-t1)   
+
+        VALUE=LDOS_IN
+        ldos=VALUE*system.get_volume()
+        print_mpi(rank, 'Searching for ISO '+str(VALUE)+' e/A^3')
+        if (ldos < min) or (ldos > max):
+            print_mpi(rank, 'Error: iso value must be in range ['+str(min/system.get_volume())+','+str(max/system.get_volume())+']')
+        else:
+            fileout=open('data.'+str(rank),'w')
+
+            ### Parallel this part
+            #Arranging point for each cpu
+            print_mpi(rank, 'Arranging data ...') 
+            t1=datetime.now()
+
+            npts=total.shape[0]*total.shape[1]  #Total point
+            ppc=int(npts/size)                  #point per cpu
+            pre=npts-ppc*size                   #remaining point
+            rppc=np.zeros(size,dtype=int)       #real ppc 
+            rppc[:]=ppc
+            if pre > 0:
+                for i in range(pre):
+                    rppc[i]+=1
+            for i in range(size):
+                print_mpi (rank, 'CPU '+str(i)+' calculates '+str(rppc[i])+' points')
+            data_point=np.zeros([npts,2],dtype=int)
+            data=np.zeros([npts])
+            for xx in range(total.shape[0]):
+                for yy in range(total.shape[1]):
+                    i=xx*total.shape[1]+yy
+                    data_point[i,0]=xx
+                    data_point[i,1]=yy
+            #Assign index for each cpu
+            start_index=np.zeros(size,dtype=int)
+            end_index=np.zeros(size,dtype=int)
+            start_index[0]=0
+            end_index[0]=rppc[0]-1
+
+            for i in range(1,size):
+                for j in range(i):
+                    start_index[i]+=rppc[j]
+                end_index[i]=start_index[i]+rppc[i]-1   
+            for i in range(size):
+                print_mpi(rank, str(i)+' '+str(start_index[i])+' '+str(end_index[i])) 
+            t2=datetime.now()
+            print_time (rank, t2-t1)
+
+            print_mpi (rank, 'SCANNING...')
+            t1 = datetime.now()
+
+            for i in range(start_index[rank],end_index[rank]+1):
+                xx=data_point[i,0]
+                yy=data_point[i,1]
+                for zz in range(ng[2]-1+TOP,-1,-1):
+                    if zz == 0:
+                        print >>fileout,xx,yy,'0.'
+                        data[i]=0.-ref
+                    else:
+                        if (total[xx,yy,zz]<=ldos) and (total[xx,yy,zz-1]>=ldos):
+                            zarray=[zz-3,zz-2,zz-1,zz,zz+1,zz+2]
+                            darray=np.take( total [ xx,yy, :] , zarray ) 
+                            f3=interp1d(zarray,darray,kind='cubic')
+                            zz1=np.arange(zz-1,zz,0.0001)
+                            dd1=f3(zz1)
+                            dist = ( dd1 - ldos ) ** 2 
+                            min_indx = dist.argmin ()
+                            z1 = zz1 [min_indx]
+  
+                            print>>fileout,xx,yy,z1
+                            data[i]=z1/float(total.shape[2]/2.)-ref
+                            break
+
+            t2 = datetime.now()
+            fileout.close()
+            print 'CPU '+str(rank)+' TIME '+str(t2-t1)
+            comm.Barrier()
+            # Broadscating data
+            print_mpi(rank, 'Broadscating data...',endline=False)
+            t1=datetime.now()
+            tmp_data = np.zeros([npts])
+            if rank == 0:
+                for i in range(1, size):
+                    comm.Recv(tmp_data, source= MPI.ANY_SOURCE)
+                    data += tmp_data
+            else:
+                comm.Send(data,dest=0)
+            comm.Barrier()
+            t2=datetime.now()
+            print_time(rank, str(t2-t1))
+            
+            if rank == 0:
+                stm=np.zeros([total.shape[0],total.shape[1],1])
+                for xx in range(total.shape[0]):
+                    for yy in range(total.shape[1]):
+                        i = xx*total.shape[1] + yy
+                        stm[xx,yy,0]=data[i]
+
+                write_chgcar(OUTPRE+'.STM.vasp',system,data=stm)
+
+            comm.Barrier()
+
---
+++ vasp/stm.py
@@ -0,0 +1,191 @@
+'''
+Has not been cleaned up
+'''
+"""
+***********************************
+Tersoff-Hamann approximation of STM
+
+Usage:
+from dlescripts.getstm import tersoff_hamann
+
+tersoff_hamann('PARCHG','OUTPUT_PREFIX',TOP,LDOS_IN,ref)
+
+************************************
+"""
+#!/usr/bin/env python
+from ase import *
+from ase.io import *
+import numpy as np
+from dlePy.vasp.chgcar import *
+from dlePy.small_tools import file_exist
+from scipy.interpolate import interp1d
+#from datetime import datetime
+
+def tersoff_hamann(CONTCAR,INDATA,OUTPRE,TOP,LDOS_IN,ref):    
+    print '***********************************'
+    print 'Tersoff-Hamann approximation of STM'
+    print ''
+    print 'The constant current STM image is  '
+    print 'simulated for the following:'
+    print ''    
+    print 'Input data: ',INDATA
+    print 'Value of LDOS: ',LDOS_IN,' e/A^3'
+    print 'The program will add ',TOP,' point'
+    print 'in z directions'
+    print 'STM image in CHGCAR format is written'
+    print 'in ',OUTPRE+'.STM.vasp'
+    print 'and in 2D crystal coordinates in: ',OUTPRE+'.STM.vasp'
+    print ''
+    print '************************************'
+    print ''
+    print '' 
+
+    #Read CONTCAR to define the system.
+    print "checking file: "+CONTCAR
+    file_exists, txt=file_exist(CONTCAR)
+    if not file_exists:
+        print txt
+    else:
+        print "checking file: "+INDATA
+        file_exists, txt=file_exist(INDATA)
+        if not file_exists:
+            print txt
+    if file_exists:
+        system=read(CONTCAR)
+        total0 = read_chgcar(INDATA,CONTCAR=CONTCAR)
+        ng = total0.shape
+        min=np.min(total0)
+        max=np.max(total0)
+        print 'Min ldos ',min,' e/A^3'
+        print 'Max ldos ',max,' e/A^3'
+        total = np.zeros([ng[0],ng[1],ng[2] + TOP])
+        total[:,:,0:ng[2]]=total0[:,:,0:ng[2]]
+        total[:,:,ng[2]:ng[2]+TOP]=total0[:,:,0:TOP]
+
+        # deallocate total0 for saving memory
+        del total0   
+ 
+        ldos = LDOS_IN  
+        print 'Searching for ISO ',LDOS_IN, ' e/A^3'
+        if (ldos < min) or (ldos > max):
+            print 'Error: iso value must be in range [',\
+                   min,\
+                   max,\
+                   ']'
+        else:
+            fileout=open(OUTPRE+'.STM','w')
+            data=np.empty([total.shape[0],total.shape[1],1])
+            for xx in range(total.shape[0]):
+                for yy in range(total.shape[1]):
+                    print 'Scanning :', xx, yy
+                    col_xy = total [xx,yy,:]
+                    zz = col_xy.shape[0] -  np.argmax ( col_xy[::-1] >= ldos ) -1
+                    if zz == 0:
+                        print >>fileout,xx,yy, '0.'
+                        data[xx,yy,0]=0.
+                    else:
+                        zarray = [ zz-2, zz-1, zz, zz+1, zz+2, zz+3] 
+                        darray = np.take ( col_xy , zarray )
+                        f3=interp1d(zarray,darray,kind='cubic')
+                        zz1=np.arange(zz,zz+1,0.0001)
+                        dd1=f3(zz1)
+                        dist = ( dd1 - ldos ) ** 2 
+                        min_indx = dist.argmin ()
+                        z1 = zz1 [min_indx]
+                        print >>fileout,xx/float(ng[0]),\
+                                        yy/float(ng[1]),\
+                                        z1/float(ng[2])-ref
+                        data[xx,yy,0]=z1/float(ng[2]) - ref
+ 
+            write_chgcar(OUTPRE+'.STM.vasp',system,data=data)
+            fileout.close()
+
+
+def convert_2D_to_3D(DATA2D,system,DATA3D):
+    """
+    Coded on 02/22/2013
+    """
+    file_exists, txt=file_exist(DATA2D)
+    if not file_exists:
+        exit(txt)
+    file_exists, txt=file_exist(DATA3D)
+    if file_exists:
+        exit('File '+DATA3D+' exist. Use different filename or remove '+DATA3D+'.')
+    #del system.constraints
+    print '***********************************'
+    print 'Converting 2D (three column data) STM to'
+    print '3D (in CHGCAR format) STM.'
+    print ''
+    print '2D Data: ',DATA2D
+    print '3D output: ',DATA3D
+    print '************************************'
+    print ''
+    print ''
+    
+    f=np.loadtxt(DATA2D)
+    x=f[:,0]
+    y=f[:,1]
+    z=f[:,2]
+    
+    ndata=len(x)
+    #Counting NY
+    ny=0
+    for i in range(ndata):
+        if x[i]==0.:
+            ny+=1
+    nx = int (ndata / ny )
+
+    if ndata !=  (ny * nx):
+        exit('Someting went wrong. The estimated values of nx, ny are not correct')    
+
+    data = np.zeros([nx,ny,1])
+    item = 0
+    for i in range(nx):
+        for j in range(ny):
+            data[i,j,0] = z[item]
+            item += 1
+    write_chgcar(DATA3D,system,data=data)
+
+
+def expand_2D(DATA2D,system,OUT,n, m):
+    """
+    Coded on 04/10/2015
+    """
+    file_exists, txt=file_exist(DATA2D)
+    if not file_exists:
+        file_exists_gz, txt_gz=file_exist(DATA2D+'.gz')
+        if not file_exists_gz:
+            exit(txt +'\n' + txt_gz)
+        else:
+            DATA2D +='.gz'
+            
+    file_exists, txt=file_exist(OUT)
+    if file_exists:
+        exit('File '+OUT+' exist. Use different filename or remove '+OUT+'.')
+    #del system.constraints
+    print '***********************************'
+    print 'Expand 2D (three column data) STM '
+    print ''
+    print '2D Data: ',DATA2D
+    print 'Output: ',OUT
+    print 'Expand: ', n, 'x ', m
+    print '************************************'
+    print ''
+    print ''
+    
+    f=np.loadtxt(DATA2D)
+    ix=f[:,0]
+    iy=f[:,1]
+    iz=f[:,2]
+    
+    ndata=len(ix)
+    fout=open(OUT, 'w')
+    for i in range(ndata):
+        xn = np.array([ix[i],iy[i],iz[i]])
+        x  = np.dot (xn, system.cell)
+        for j in range(-1,n):
+            for k in range(-1, m):
+                x_out = x + j*system.cell[0,:] + k*system.cell[1,:]
+                print >>fout, "%15.10f %15.10f %15.10f" %(x_out[0], x_out[1], x_out[2])
+    fout.close()
+        
