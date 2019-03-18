79ddc902a3feae53d2597f184df40140a13464a9
Duy Le <ttduyle@gmail.com>
2019-03-18 11:43:21 -0400

    simplified

--- math.py
+++ math.py
@@ -2,21 +2,8 @@ from   ase.io import read
 import numpy as np
 import os
 
-
-def dotproduct( v1, v2 ):
-    return sum( ( a * b ) for a, b in zip( v1, v2 ) )
-
-def length( v ):
-    return np.sqrt(dotproduct(v, v))
-
-def area( v1, v2 ):
-    '''
-    calculate area created by two vectors
-    '''
-    return length( np.cross( v1, v2 ) )
-
-def str2bool( s ):
-    return s.lower() in ( "yes", "true", "t", "1" )
-
-def tripleproduct( v1, v2, v3 ):
-    return dotproduct( v3, np.cross( v1, v2 ) )
+dotproduct    = lambda v1, v2    : np.dot (v1, v2 ) 
+length        = lambda v         : np.linalg.norm( v )
+area          = lambda v1, v2    : length( np.cross(v1, v2 ) )
+str2bool      = lambda s         : s.lower() in ( "yes", "true", "t", "1" )
+tripleproduct = lambda v1, v2, v3: dotproduct( v3, np.cross(v1, v2 ) )
