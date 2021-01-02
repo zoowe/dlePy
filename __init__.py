cfa922fade93d7ad31505a9a4a6d01db584e0d7a
Duy Le <ttduyle@gmail.com>
2021-01-02 16:52:46 -0500

    add setup.py

--- __init__.py
+++ __init__.py
@@ -2,7 +2,7 @@ import sys
 
 str='''
 dlePy V 0.2         
-Updated: 08/23/2018 
+Updated: 01/02/2021 
 
 Author: Duy Le
 Department of Physics
@@ -19,10 +19,3 @@ print ( str )
 if sys.version_info[ 0 ] == 2:
     raise ImportError( 'dlePy requires Python3. This is Python2.' )
 
-"""
-Cleaning log:
-06/06/2018: strucmod.py and supercell.py 
-
-Update log:
-Disregard all log before 06/06/2018.
-"""
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name         = 'dlePy',
+      version      = '0.2',
+      description  = 'dlePy is a small collection of python scripts used for creating, modifying atomic structures for computational simulations and for analysing results.',
+      url          = 'https://github.com/zoowe/dlePy',
+      author       = 'Duy Le',
+      author_email = 'duy.le@ucf.edu',
+      license      = 'GNU General Public License v3.0',
+      packages     = ['dlePy'],
+      zip_safe     = False)
